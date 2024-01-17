137b9d4c9399c45c12ec80a3d1d360bc2ba5fa0f
anusha7530 <agarwalanusha13@gmail.com>
2024-01-17 19:27:52 +0530

    Update files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>
   </React.StrictMode>,
 )
